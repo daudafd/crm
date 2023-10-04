48c782f20502c69e4fd91806e79bfd2790439e78
daudafd <daudafd@gmail.com>
2023-10-04 15:49:12 +0100

    image upload to user peofile

--- crm/settings.py
+++ crm/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
